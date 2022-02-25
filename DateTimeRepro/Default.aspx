b9475267b8f74e33bd7a821dd528b9be4a442ec0
vtripathi <vtripathi@DESKTOP-LJ1T0GG>
2022-02-25 14:47:46 -0500

    Add project files.

---
+++ DateTimeRepro/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DateTimeConvertTest._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="my-4">
+        <h3>Current culture</h3>
+        <asp:Literal runat="server" ID="currentCulture" EnableViewState="false" />
+        <h3>Convert Result</h3>
+        <asp:Literal runat="server" ID="convertResult" EnableViewState="false" />
+    </div>
+</asp:Content>
---
+++ DateTimeRepro/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Web.UI;
+
+namespace DateTimeConvertTest
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            currentCulture.Text = CultureInfo.CurrentCulture.Name;
+
+            var date = "05/27/22";
+
+            try
+            {
+                convertResult.Text = Convert.ToDateTime(date).ToString();
+            }
+            catch (Exception ex)
+            {
+                convertResult.Text = ex.ToString();
+            }
+        }
+    }
+}
